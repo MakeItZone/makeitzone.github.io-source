419e306160cbf3e68188ff536b219d3609b8c5d9
Julian Rendell <juliangrendell@gmail.com>
2016-12-31 16:19:45 -0800

    making subcommands more verbose

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # One script for command line and Travis use
 
 ## NOTE: Assumes you've already set up and activated at python (3.6) virtualenv
-pip install -r requirements.txt
+pip install -v -r requirements.txt
 cd src/pelican
-python setup.py install
+python setup.py -v install
 cd ../../
