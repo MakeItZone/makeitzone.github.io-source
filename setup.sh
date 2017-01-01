dc93fcd6b03d5d3420cd268bf3b52201d5b0e2d2
Julian Rendell <juliangrendell@gmail.com>
2016-12-31 16:38:18 -0800

    lets get the build green...

--- setup.sh
+++ setup.sh
@@ -2,7 +2,12 @@
 # One script for command line and Travis use
 
 ## NOTE: Assumes you've already set up and activated at python (3.6) virtualenv
+
+echo "====== Installing Pip requirements ======"
+
 pip install -r requirements.txt
+
+echo "====== Installing Pelican from Source ======"
 cd src/pelican
 python setup.py install
 cd ../../
